6ff562687208ff22986d72f8d357c631b6fc2b29
Somil Agrawal <somilagrawal680@gmail.com>
2022-08-23 11:26:38 +0530

    update

---
+++ app.js
@@ -0,0 +1,77 @@
+
+
+function opennav(){
+    document.getElementById("mynav").style.width = "100%";
+    console.log("click")
+}
+function closenav(){
+    document.getElementById("mynav").style.width = "0%";
+    console.log("clickeddd")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const darkmode = document.querySelector("#page-darkmode-btn");
+// const mainbody = document.querySelector("#mainbody")
+// const on = document.querySelector(".fa-solid fa-moon")
+// const off = document.querySelector(".fa-solid fa-sun")
+
+// darkmode.addEventListener("click" , darkmodeon)
+
+// function darkmodeon(){
+//     console.log("click")
+//     on.style.color = "red";
+//     off.style.display = "block "
+// }
+
+
+// darkmode.addEventListener('click' , darkmodeon )
+// function darkmodeon(){
+//     mainbody.style.backgroundColor = "black";
+//     mainbody.style.color = "white";
+//     // body.style.border = "black"
+    
+// }
+// darkmode.addEventListener('click' , brightmode )
+// function brightmode(){
+//     mainbody.style.backgroundColor = "white";
+//     mainbody.style.color = "black";
+//     // body.style.border = "black"
+
+// }
